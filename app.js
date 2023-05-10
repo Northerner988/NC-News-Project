61817b945f12e1c8b8ffc2a60b86d16f2b4694dc
Northerner988 <lulu12100@hotmail.co.uk>
2023-05-10 08:48:27 +0100

    add psql error handling

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 const express = require("express");
 const { getAllTopics } = require("./controllers/topics.controller");
+const { getArticleById } = require("./controllers/articles.controller");
 const app = express();
 
 app.get("/api/topics", getAllTopics);
+
+app.get("/api/articles/:article_id", getArticleById);
+
+app.all("*", (req, res) => {
+  res.status(404).send({ msg: "Route not found" });
+});
+
+app.use((err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Bad request - ID is invalid" });
+  }
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status).send({ msg: err.msg });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ msg: "Internal server error" });
+});
 
 module.exports = app;
