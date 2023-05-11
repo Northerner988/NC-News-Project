6f423cfb66822f73514159f0be76e23d5720bc80
Northerner988 <lulu12100@hotmail.co.uk>
2023-05-11 08:35:47 +0100

    setup controller

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,4 +1,7 @@
-const { fetchArticleById } = require("../models/articles.model");
+const {
+  fetchArticleById,
+  fetchCommentsById,
+} = require("../models/articles.model");
 
 exports.getArticleById = (req, res, next) => {
   const { article_id } = req.params;
@@ -6,6 +9,18 @@ exports.getArticleById = (req, res, next) => {
   fetchArticleById(article_id)
     .then((specifiedArticle) => {
       res.status(200).send({ article: specifiedArticle });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.getCommentsById = (req, res, next) => {
+  const { article_id } = req.params;
+
+  fetchCommentsById(article_id)
+    .then((comments) => {
+      res.status(200).send({ comments: comments });
     })
     .catch((err) => {
       next(err);
