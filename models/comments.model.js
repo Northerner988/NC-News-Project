474580eb178d692052d443181291edc7b5fd9d93
Northerner988 <lulu12100@hotmail.co.uk>
2023-05-27 20:02:37 +0100

    setup model for patch request to /comments/:comment_id endpoint

--- models/comments.model.js
+++ models/comments.model.js
@@ -11,3 +11,18 @@ exports.removeCommentById = (comment_id) => {
     );
   });
 };
+
+exports.updateCommentById = (comment_id, inc_votes) => {
+  return checkCommentExists(comment_id)
+    .then(() => {
+      return db.query(
+        `UPDATE comments
+        SET votes = votes + $1
+        WHERE comment_id = $2 RETURNING *;`,
+        [inc_votes, comment_id]
+      );
+    })
+    .then((result) => {
+      return result.rows[0];
+    });
+};
