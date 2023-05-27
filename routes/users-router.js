50496c257664a0f1af3143f96b457c6e4a54a530
Northerner988 <lulu12100@hotmail.co.uk>
2023-05-27 18:40:06 +0100

    create router for users/:username endpoint

--- routes/users-router.js
+++ routes/users-router.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
 
-const { getAllUsers } = require("../controllers/users.controller");
+const {
+  getAllUsers,
+  getUsersById,
+} = require("../controllers/users.controller");
 
 router.get("/", getAllUsers);
-
+router.get("/:username", getUsersById);
 module.exports = router;
