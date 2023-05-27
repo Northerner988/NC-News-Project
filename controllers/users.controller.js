349d02f66a294d6fb2fa10f4aeb6687a44dec580
Northerner988 <lulu12100@hotmail.co.uk>
2023-05-27 18:38:38 +0100

    setup controller for users/:username endpoint

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,9 +1,20 @@
-const { fetchAllUsers } = require("../models/users.model");
+const { fetchAllUsers, fetchUserById } = require("../models/users.model");
 
 exports.getAllUsers = (req, res, next) => {
   fetchAllUsers()
     .then((allUsers) => {
       res.status(200).send({ users: allUsers });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.getUsersById = (req, res, next) => {
+  const { username } = req.params;
+  fetchUserById(username)
+    .then((user) => {
+      res.status(200).send({ users: user });
     })
     .catch((err) => {
       next(err);
